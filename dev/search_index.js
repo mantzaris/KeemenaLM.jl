e784f5b46a99f1307ce0ced7dfca42b5909fd615
Documenter.jl <documenter@juliadocs.github.io>
2026-02-20 20:06:07 +0000

    build based on fd8f227

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#KeemenaLM.jl","page":"Home","title":"KeemenaLM.jl","text":"This documentation site is a placeholder.\n\nThe repository is currently scaffolded with:\n\nCI (tests + docs build/deploy)\na minimal package entry point\na minimal test suite\n\nImplementation will be added in future commits.","category":"section"}]
+}
